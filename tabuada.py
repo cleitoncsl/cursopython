2d287ec9ae2c51b6d488a9e8ab31f16be80577d2
Cleiton <cleitoncsl@gmail.com>
2018-10-24 15:13:07 -0300

    Código funcionando.

--- tabuada.py
+++ tabuada.py
@@ -1,3 +1,6 @@
 num = int(input('Digite um numero para ver sua tabuada: '))
 x = 1
-print('{} x {} = {}'.format(num, x, num*x))
+while (x <=10):
+    print('{} x {} = {}'.format(num, x, num*x))
+    x+=1
+print('fim')
