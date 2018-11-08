2beff0815e3e91f5f7190137c6b93946256b402f
Cleiton da Silva Leal <cleitoncsl@gmail.com>
2018-11-07 22:03:34 -0200

    Teste Webcrawler

---
+++ crawler.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import lxml
+import requests
+#from lxml import html
+
+url = str(input('url: '))
+r = requests.get('{}'.format(url))
+status = r.status_code
+if status == 200:
+    print("conexão realizada com sucesso")
+else:
+    print("erro na conexão")
+tree = html.fromstring(r.content)
+cargo = (tree.xpath('//*[@id="article"]/div/div[2]/div/h1/span/text()'))[0].strip()
+salario = (tree.xpath('//*[@id="article"]/div/div[2]/div/p[5]/text()'))[0].strip()
+email = (tree.xpath('//*[@id="article"]/div/div[2]/div/p/a[2]/text()'))[0].strip()
+
+print('Cargo: {}'.format(cargo))
+print('Salário: {}'.format(salario))
+print('E-mail: {}'.format(email))
